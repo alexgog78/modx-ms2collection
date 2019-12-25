793e12bf1db3681e82b9c6607a437669c2abdcad
Aleksey <a.goguev@alexgog.ru>
2019-12-25 19:24:40 +0300

    Initial commit

---
+++ core/components/ms2collection/model/ms2collection/ms2collection.class.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!class_exists('abstractModule')) {
+    require_once MODX_CORE_PATH . 'components/abstractmodule/model/abstractmodule/abstractmodule.class.php';
+}
+
+class ms2Collection extends abstractModule
+{
+
+}
