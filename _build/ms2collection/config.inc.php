7db978665af62f28d9ed206a2b13258cf27d12ff
Aleksey <a.goguev@alexgog.ru>
2020-02-25 21:06:01 +0300

    _build scripts

--- _build/ms2collection/config.inc.php
+++ _build/ms2collection/config.inc.php
@@ -1,8 +1,12 @@
 <?php
 
-require_once dirname(dirname(__FILE__)) . '/abstractmodule/config.inc.php';
-
 define('PKG_NAME', 'ms2Collection');
 define('PKG_NAME_LOWER', 'ms2collection');
 define('PKG_VERSION', '1.0');
 define('PKG_RELEASE', 'rc1');
+define('MS2_PLUGINS', [
+    [
+        'name' => 'ms2CollectionProduct',
+        'path' => '{core_path}components/' . PKG_NAME_LOWER . '/ms2/plugins/product/index.php',
+    ],
+]);
