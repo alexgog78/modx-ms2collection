8cc4c574557a7ec69ae808023e58dbe850ed1d45
Aleksey <a.goguev@alexgog.ru>
2020-03-04 22:07:41 +0300

    Version fix, new package

--- _build/ms2collection/config.inc.php
+++ _build/ms2collection/config.inc.php
@@ -2,5 +2,5 @@
 
 define('PKG_NAME', 'ms2Collection');
 define('PKG_NAME_LOWER', 'ms2collection');
-define('PKG_VERSION', '1.0');
-define('PKG_RELEASE', 'rc1');
+define('PKG_VERSION', '1.0.0');
+define('PKG_RELEASE', 'beta');
